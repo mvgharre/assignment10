7b84560eaf8c888dcce3163ef549b9bc5a599143
hari prasad <mvgharre@gmail.com>
2015-12-18 19:02:59 -0600

    first commit

---
+++ app/src/main/java/com/example/hariprasad/assignment10/GameView.java
@@ -0,0 +1,202 @@
+package com.example.hariprasad.assignment10;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
+import android.text.method.DialerKeyListener;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+/**
+ * Created by 984707 on 12/3/2015.
+ */
+public class GameView extends SurfaceView {
+    SurfaceHolder holder;
+    Bitmap maharishi_mahesh_yogi;
+    private int score = 0;
+    private long timer = 0;
+    private Paint scorePaint;
+    // horizontal position (graphic is 205 pixels wide thus initialize right edge of graphic fall to left screen edge)
+    private float maharishi_mahesh_yogiX = 205.0f;
+    private float maharishi_mahesh_yogiY = 100.0f;
+    private GameThread gthread = null;
+    int min = 100;
+    int max = 300;
+    int min1 = 100;
+    int max2 = 400;
+    int startTime = 20000;
+    int intervalTime = 1000;
+    CountDownTimer maintimer;
+    SharedPreferences settings;
+
+
+    public GameView(final Context context) {
+        super(context);
+        holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                settings = context.getSharedPreferences("PREFERENCE", context.MODE_PRIVATE);
+
+
+                // Reading from SharedPreferences
+
+
+                maharishi_mahesh_yogi = BitmapFactory.decodeResource(getResources(), R.drawable.maharishi_mahesh_yogi);
+                scorePaint = new Paint();
+                scorePaint.setColor(Color.WHITE);
+                scorePaint.setTextSize(30.0f);
+                makeThread();
+
+                maintimer = new CountDownTimer(startTime, intervalTime) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        timer = millisUntilFinished / 1000;
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        // Writing data to SharedPreferences
+                        SharedPreferences.Editor editor = settings.edit();
+                        if (score>Integer.parseInt(settings.getString("key", "0"))  ) {
+                            editor.putString("key", score + "");
+                        }
+                        editor.commit();
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+
+                        builder
+                                .setMessage("Your score :" + settings.getString("key", ""));
+
+
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                                score = 0;
+                                makeThread();
+                                maintimer.start();
+
+                                gthread.start();
+                            }
+                        });
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                                killThread();
+                                ((Activity) context).finish();
+                            }
+                        });
+                        builder.show();
+
+
+                    }
+                };
+                maintimer.start();
+
+                gthread.setRunning(true);
+                gthread.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+
+
+        });
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //  mouseX = mouseX + 4.0f;
+
+
+
+
+
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(maharishi_mahesh_yogi, maharishi_mahesh_yogiX, maharishi_mahesh_yogiX, null);
+
+
+        canvas.drawText("Max: " + settings.getString("key", ""), 150.0f, 50.0f, scorePaint);
+        canvas.drawText("Score: " + String.valueOf(score), 10.0f, 50.0f, scorePaint);
+
+        canvas.drawText("TIME:" + timer, getWidth() - 200.0f, 50.0f, scorePaint);
+
+    }
+
+    public void makeThread() {
+        gthread = new GameThread(this);
+
+    }
+
+    public void killThread() {
+        boolean retry = true;
+        gthread.setRunning(false);
+        while (retry) {
+            try {
+                gthread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void onDestroy() {
+        maharishi_mahesh_yogi.recycle();
+        maharishi_mahesh_yogi = null;
+        System.gc();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        Random r = new Random();
+        float i1 = r.nextInt(max - min + 1) + min;
+        float i2 = r.nextInt(max - min + 1) + min;
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            if (x > maharishi_mahesh_yogiX && x < maharishi_mahesh_yogiX + maharishi_mahesh_yogi.getWidth() && y > maharishi_mahesh_yogiX && y < maharishi_mahesh_yogiX + maharishi_mahesh_yogi.getHeight()) {
+                Log.e("Coordinate", i1 + " " + i2);
+                maharishi_mahesh_yogiX = i1;
+                maharishi_mahesh_yogiX = i2;
+
+                score++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hariprasad/assignment10/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.hariprasad.assignment10;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+GameView gameView;
+    private Button flowerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+////
+
+        FrameLayout GameLayout = new FrameLayout(this);
+        LinearLayout buttonlayout = new LinearLayout(this);
+
+
+
+        GameLayout.addView(gameView);
+        GameLayout.addView(buttonlayout);
+        setContentView(GameLayout);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.killThread();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        gameView.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
+}
